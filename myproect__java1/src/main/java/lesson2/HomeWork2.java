cd58106578e967f9ec4562eed79b9493ef29ff68
borzova_da <d_bushueva@mail.ru>
2021-10-03 20:39:28 +0500

    @wДомашнее задание №2 se enter the commit message for your changes. Lines starting

--- myproect__java1/src/main/java/lesson2/HomeWork2.java
+++ myproect__java1/src/main/java/lesson2/HomeWork2.java
@@ -3,28 +3,42 @@ package lesson2;
 import java.util.zip.Checksum;
 
 public class HomeWork2 {
-    public static void main(String[] args){
-        CheckSum();
-        CheckVar();
+    public static void main(String[] args) {
+        CheckSum(11, 2);
+        CheckVar(-89);
+        isNegative(7);
+        printWords("Печатаем строку", 5);
     }
 
-    public static void CheckSum() {
-        int VarA=20;
-        int VarB=40;
-        int Sum=(VarA + VarB);
-        if (Sum >=10 && Sum <= 20){
-            System.out.println("True");
+    public static void CheckSum(int x, int y) {
+        int sum = x + y;
+        if ((sum >= 10) && (sum <= 20)) {
+            System.out.println("true");
         } else {
-            System.out.println("False");
+            System.out.println("false");
         }
     }
 
-    public static void CheckVar() {
-        int VarA=67;
-        if (VarA >= 0){
+    public static void CheckVar(int x) {
+        if (x >= 0) {
             System.out.println("Положительное");
         } else {
             System.out.println("Отрицательное");
         }
     }
+
+    public static void isNegative(int x) {
+        if (x >= 0) {
+            System.out.println("false");
+        } else {
+            System.out.println("true");
+        }
+    }
+
+    public static void printWords(String words, int times) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(words);
+        }
+    }
 }
+
