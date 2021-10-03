d446d22b20477cd07576e4eb6d9533d42eb1d551
borzova_da <d_bushueva@mail.ru>
2021-10-03 21:56:45 +0500

    Последние правки

--- myproect__java1/src/main/java/lesson2/HomeWork2.java
+++ myproect__java1/src/main/java/lesson2/HomeWork2.java
@@ -4,18 +4,18 @@ import java.util.zip.Checksum;
 
 public class HomeWork2 {
     public static void main(String[] args) {
-        CheckSum(11, 2);
-        CheckVar(-89);
+       CheckSum(11, 2);
+       CheckVar(-89);
         isNegative(7);
-        printWords("Печатаем строку", 5);
+        printWords("Печатаем строку", 4);
     }
 
-    public static void CheckSum(int x, int y) {
+    public static boolean CheckSum(int x, int y) {
         int sum = x + y;
         if ((sum >= 10) && (sum <= 20)) {
-            System.out.println("true");
+            return true;
         } else {
-            System.out.println("false");
+            return false;
         }
     }
 
@@ -27,16 +27,16 @@ public class HomeWork2 {
         }
     }
 
-    public static void isNegative(int x) {
+    public static boolean isNegative(int x) {
         if (x >= 0) {
-            System.out.println("false");
+            return false;
         } else {
-            System.out.println("true");
+            return true;
         }
     }
 
     public static void printWords(String words, int times) {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < times; i++) {
             System.out.println(words);
         }
     }
